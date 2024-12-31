463da823e2a0d34e44371972dd62d09b005d7516
CDOM201 <carlos@carlosdaniel.info>
2024-12-31 01:24:45 -0700

    Update mongo.js

--- util/database/mongo.js
+++ util/database/mongo.js
@@ -3,16 +3,21 @@ const mongoose = require('mongoose');
 
 module.exports = {
     init: async () => {
-        const dbOptions = {};
-        mongoose.connect(process.env.MONGODB_URL, dbOptions);
-        mongoose.Promise = global.Promise;
-        
-        mongoose.connection.on('error', (err) => {
+        try {
+            const dbOptions = {};
+            mongoose.connect(process.env.MONGODB_URL, dbOptions);
+            mongoose.Promise = global.Promise;
+            
+            mongoose.connection.on('error', (err) => {
+                console.error('MongoDB Connection Error: ', err);
+            });
+            mongoose.connection.on('connected', () => {
+                console.log('MongoDB Connected');
+            });
+        }        
+        catch (err) {
             console.error('MongoDB Connection Error: ', err);
-        });
-        mongoose.connection.on('connected', () => {
-            console.log('MongoDB Connected');
-        });
+        }
     },
     getDb: () => {
         return mongoose.connection;
