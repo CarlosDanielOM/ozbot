5eb5c8e8a42d1bc5996a88ca5a7fa4a0b8a6d618
CDOM201 <carlos@carlosdaniel.info>
2025-02-05 15:08:29 -0700

    Fixed the overlap of attendance and daily

--- handlers/daily.js
+++ handlers/daily.js
@@ -4,7 +4,24 @@ const attendanceSchema = require('../models/attendance');
 
 async function daily() {
     let cacheClient = getClient();
+
+    //! Adds a 10 second delay in order to let attendance update first before deleting and savin in the database
+    await new Promise(resolve => setTimeout(resolve, 10000));
+
+    //? Put in users that did not attend the stream
+    let list = await cacheClient.smembers('oz:list');
     let keys = await cacheClient.keys('oz:assistance:*');
+
+    keys = keys.map(key => key.split(':')[2]);
+    list = list.filter(user => !keys.includes(user));
+
+    for(let i = 0; i < list.length; i++) {
+        let user = list[i];
+
+        await cacheClient.set(`oz:assistance:${user}`, 0);
+    }
+    
+    keys = await cacheClient.keys('oz:assistance:*');
 
     let logins = keys.map(key => key.split(':')[2]);
 
